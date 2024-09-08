d437dcf85ba2b50ef8353ee77a216511dc239ca9
Prabhu Kasinathan <16215481+pkasinathan@users.noreply.github.com>
2024-09-08 02:15:25 +0000

    Initial Version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,97 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Set the title of the website
+st.title("Welcome to Idris Ahamed's World!")
+
+# Home Page
+st.header("About Me")
+st.write("""
+Hello there! 👋 My name is Idris Ahamed. I'm 9 years old and I study at Lincoln Elementary School in Newark, California. 
+Let me tell you a little about myself. 
+
+I love typing! 🖥️ It's so much fun to see how fast I can type and how many words I can type in a minute. 
+When I'm not typing, I enjoy playing games, hanging out with my friends, and exploring new things.
+
+My favorite subjects in school are Math and Science because I love solving problems and learning how things work. 
+I also enjoy reading books, especially adventure stories. 📚
+
+On weekends, I like to go to the park with my family, ride my bike, and sometimes we even have a picnic! 🧺
+
+I'm always curious and love learning new things. I think that when I grow up, I might want to be a software developer, 
+so I can create amazing games and apps that everyone can enjoy!
+
+Thanks for visiting my website! Scroll down to learn more about my hobbies, interests, and some cool stuff I've been up to!
+""")
+
+# Hobby Section
+st.header("My Favorite Hobby: Typing!")
+st.write("""
+Typing is so much fun! I can spend hours just practicing my typing skills. 
+Here are some reasons why I love typing:
+
+1. **It's like a race:** Every time I type, I try to beat my previous speed. I love challenging myself to type faster and more accurately.
+2. **It's creative:** I can write stories, poems, and even codes! Typing allows me to put my imagination into words.
+3. **It's useful:** Typing helps me with my schoolwork and allows me to communicate with friends and family online.
+
+I have some favorite typing games that I play to improve my speed. Some of them are TypingClub, NitroType, and Dance Mat Typing. 
+If you want to become a typing master like me, you should check them out! 🚀
+""")
+
+# School Section
+st.header("My School Life")
+st.write("""
+I go to Lincoln Elementary School, which is a really cool place to learn. 
+Our teachers are super nice, and I have a lot of friends there. 
+
+Here are some of my favorite things about school:
+
+- **Math Class:** I love solving puzzles and learning new math tricks.
+- **Science Experiments:** We do a lot of hands-on experiments in science class. Last week, we made a volcano explode using baking soda and vinegar!
+- **Recess:** Recess is the best time of the day because I get to play with my friends. We usually play soccer, tag, or just run around having fun.
+
+I also enjoy art class because I can get creative with colors and shapes. Sometimes, we even get to make things out of clay!
+""")
+
+# Interests Section
+st.header("My Interests")
+st.write("""
+Apart from typing, I have a lot of other interests. Here's a quick list:
+
+- **Reading Books:** I love reading adventure stories. My favorite book series is 'Percy Jackson.' 
+  The adventures of demigods are so thrilling!
+- **Playing Games:** I enjoy playing video games like Minecraft and Roblox. Building new worlds and completing challenges is always exciting.
+- **Biking:** I have a cool bike, and I love riding it around the neighborhood. It's a great way to stay active and have fun.
+
+I also like drawing and sometimes, I create comic strips with my own characters. Maybe one day, I'll publish a comic book!
+""")
+
+# Future Goals Section
+st.header("What I Want to Be When I Grow Up")
+st.write("""
+When I grow up, I want to be a software developer. 
+Why? Because I love working with computers and creating new things. 
+
+I want to learn more about coding, so I can make my own games and apps. 
+Maybe I'll even invent a game that becomes super popular! Who knows? The sky's the limit! 🌟
+
+But before that, I need to learn a lot more and practice my skills. 
+I’m excited about the future and can’t wait to see what I can achieve!
+""")
+
+# Add some fun facts
+st.header("Fun Facts About Me")
+st.write("""
+Here are some fun facts about me:
+
+1. I can type 50 words per minute! ⌨️
+2. I once built a Lego castle with over 1000 pieces! 🏰
+3. I can solve a Rubik's Cube in under 2 minutes! 🎯
+4. My favorite color is blue because it reminds me of the sky and the ocean. 🌊
+5. I have a pet fish named Bubbles. He's a goldfish and loves swimming around his tank all day. 🐟
+
+Thanks for spending time on my website! I hope you enjoyed learning more about me. 
+Don't forget to come back for more updates on my latest adventures and projects!
+""")
+
+# Footer
+st.write("### Thanks for visiting my website! See you soon! 😄")
